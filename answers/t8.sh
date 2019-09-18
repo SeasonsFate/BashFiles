338817ed8f10acee39482dcb3371ed84d5488a42
= <=>
2019-09-18 23:33:09 +0000

    fixed the issues with fizz files not grepping

--- answers/t8.sh
+++ answers/t8.sh
@@ -2,7 +2,7 @@
 
 fizzbuzz 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 42 99 100 non-numeric-argumeants
 
-cd /home/$(whoami)/BashFiles/assessment/data/
-cat output | grep "fizz " >> fb/fizz
-cat output | grep " buzz" >> fb/buzz
-cat output | grep "fizzbuzz" >> fb/fizzbuzz
+
+cat /home/$(whoami)/BashFiles/assessment/data/output | grep "fizz " >> /home/$(whoami)/BashFiles/assessment/data/fb/fizz
+cat /home/$(whoami)/BashFiles/assessment/data/output | grep " buzz" >> /home/$(whoami)/BashFiles/assessment/data/fb/buzz
+cat /home/$(whoami)/BashFiles/assessment/data/output | grep "fizzbuzz" >> /home/$(whoami)/BashFiles/assessment/data/fb/fizzbuzz
